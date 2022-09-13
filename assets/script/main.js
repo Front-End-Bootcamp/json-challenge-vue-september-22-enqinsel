1be9dbe5df2e7d18414da4e8d70068b555886bca
enqinsel <enqinsel@gmail.com>
2022-09-13 17:32:09 +0300

    homework done

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,31 @@
-console.log('👻');
+import dataJson from '/data.json' assert {type: 'json'};
+
+
+function getAssistant(groupName) {
+    let assistantArray = dataJson.filter(function (filt) {
+        return filt.type === "assistant"
+    })
+    return assistantArray.find(function (fin) { return fin.group === groupName })
+}
+
+
+function getStudentsName(groupName) {
+    let studentsArray = dataJson.filter(function (filt) {
+        return filt.type !== "assistant" && filt.group === groupName
+    })
+    return studentsArray.map(function (maP) { return maP.name })
+}
+
+
+function getGroup(groupName) {
+    let assistant = getAssistant(groupName);
+    let students = getStudentsName(groupName);
+
+    return {
+        group: groupName,
+        students: students,
+        assistant: assistant.name,
+    };
+}
+
+console.log(getGroup("SteelBlue"));
